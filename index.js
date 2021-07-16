79c29ac61710c4be83cdad753912919e1b42bec6
VlasBochkarev <vlas.bochkarev@gmail.com>
2021-07-16 14:37:56 +0300

    push

---
+++ index.js
@@ -0,0 +1,15 @@
+// function User(name, age, sex) {
+//     this.userName = name;
+//     this.age = age;
+//     this.sex = sex;
+//     this.voice = function() {
+//         console.log('Hi my name is ' + this.userName + " I'am " + this.age + ' year old. I am ' + this.sex + " i can " + this.skill);
+//     }
+// }
+
+// User.skill = "driver";
+
+// let userVlas = new User("Vlas", 28, 'male')
+// userVlas.voice()
+// let userLida = new User("Lida", 21, 'girl')
+// userLida.voice()
